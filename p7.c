49f521f1c9c43b0385fca47dde88d2f2e963727f
Caitlin Coffman <caitlincoffman@gmail.com>
2023-02-26 10:43:35 -0800

    Adding p7.c

---
+++ p7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct person
+{
+    char first_name[20];
+    char last_name[20];
+    int age;
+}
+
+void print_person_info(struct person clone);
+void sort_by_age(int n, struct person a[]);
+
+int main (void) 
+{
+    int i, n=5;
+    
+    struct person students[5] = 
+    {
+        
+    }
+    
+
+}
