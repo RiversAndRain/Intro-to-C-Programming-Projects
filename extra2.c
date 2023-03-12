711922f16c1d40e99c8805635ba992025c9f6a75
Caitlin Coffman <caitlincoffman@gmail.com>
2023-03-11 17:03:30 -0800

    Extra Credit 2 (Base 10 to binary converter)

---
+++ extra2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+    unsigned long long int user_number;
+
+    printf("Enter a base_10 number from 0 --> 18,446,744,073,709,551,615: ");
+    scanf("%llu", &user_number);
+
+    int binary_num[64] = {};
+   
+    int i = 0;
+
+    for (i = 0; user_number > 0; i++)
+    {
+        binary_num[i] = user_number%2;
+        user_number = user_number/2;
+
+    }
+
+
+    for (int j = i - 1; j >= 0; j-- )
+    {
+        printf("%d",binary_num[j]);
+    }
+
+    return 0;
+}
